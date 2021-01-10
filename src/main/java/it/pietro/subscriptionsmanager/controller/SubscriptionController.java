a47980e730b65449477b07eff156965e5004557c
Pietro Zarri <pietrozarri96@gmail.com>
2021-01-10 23:09:52 +0100

    add controller

---
+++ src/main/java/it/pietro/subscriptionsmanager/controller/SubscriptionController.java
@@ -0,0 +1,20 @@
+package it.pietro.subscriptionsmanager.controller;
+
+import it.pietro.subscriptionsmanager.model.Subscription;
+import it.pietro.subscriptionsmanager.repository.SubscriptionRepository;
+import it.pietro.subscriptionsmanager.view.SubscriptionView;
+
+public class SubscriptionController {
+	
+	private final SubscriptionRepository repository;
+	private final SubscriptionView view;
+	
+	public SubscriptionController(SubscriptionRepository repository, SubscriptionView view) {
+		this.repository = repository;
+		this.view = view;
+	}
+	
+	public void allSubscriptions() {
+	}
+
+}
