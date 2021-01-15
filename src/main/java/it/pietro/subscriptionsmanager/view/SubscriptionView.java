3e6717ed2f6d63ef8749c2a58fd58ab05794bf8e
Pietro Zarri <pietrozarri96@gmail.com>
2021-01-15 10:53:08 +0100

    add pit configuration

--- src/main/java/it/pietro/subscriptionsmanager/view/SubscriptionView.java
+++ src/main/java/it/pietro/subscriptionsmanager/view/SubscriptionView.java
@@ -7,7 +7,7 @@ public interface SubscriptionView {
 	
 	void showAllSubscriptions(List<Subscription> subs);
 	
-	void showError(String Message, Subscription sub);
+	void showError(String message, Subscription sub);
 	
 	void subscriptionAdded(Subscription sub);
 	
