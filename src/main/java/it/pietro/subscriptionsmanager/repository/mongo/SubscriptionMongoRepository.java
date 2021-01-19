f8406ea12f39dd29090ef43054311b71398a874a
Pietro Zarri <pietrozarri96@gmail.com>
2021-01-19 11:01:11 +0100

    implemented SubscriptionMongoRepository class
    methods and UnitTests for SubscriptionMongoRepository

--- src/main/java/it/pietro/subscriptionsmanager/repository/mongo/SubscriptionMongoRepository.java
+++ src/main/java/it/pietro/subscriptionsmanager/repository/mongo/SubscriptionMongoRepository.java
@@ -1,17 +1,20 @@
 package it.pietro.subscriptionsmanager.repository.mongo;
 
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
 
 import com.mongodb.MongoClient;
 import org.bson.Document;
 import com.mongodb.client.MongoCollection;
+import com.mongodb.client.model.Filters;
 
 import it.pietro.subscriptionsmanager.model.Subscription;
 import it.pietro.subscriptionsmanager.repository.SubscriptionRepository;
 
 public class SubscriptionMongoRepository implements SubscriptionRepository {
 	
-	protected final MongoCollection<Document> collection;
+	private final MongoCollection<Document> collection;
 	
 	public SubscriptionMongoRepository(MongoClient client, String dbName, String collectionName) {
 		collection = client
@@ -22,25 +25,38 @@ public class SubscriptionMongoRepository implements SubscriptionRepository {
 
 	@Override
 	public List<Subscription> findAll() {
-		// TODO Auto-generated method stub
-		return null;
+		return StreamSupport
+			.stream(collection.find().spliterator(), false)
+			.map(this::fromDocumentToStudent)
+			.collect(Collectors.toList());		
+	}
+	
+	private Subscription fromDocumentToStudent(Document d) {
+		return new Subscription(""+d.get("id"), ""+d.get("name"), (Double) d.get("price"), ""+d.get("repetition"));
 	}
 
 	@Override
 	public Subscription findById(String id) {
-		// TODO Auto-generated method stub
+		Document d = collection.find(Filters.eq("id", id)).first();
+		if (d!= null) {
+			return fromDocumentToStudent(d);
+		}
 		return null;
 	}
 
 	@Override
 	public void save(Subscription sub) {
-		// TODO Auto-generated method stub
-
+		collection.insertOne(
+			new Document()
+				.append("id", sub.getId())
+				.append("name", sub.getName())
+				.append("price", sub.getPrice())
+				.append("repetition", sub.getRepetition()));
 	}
 
 	@Override
 	public void delete(String id) {
-		// TODO Auto-generated method stub
+		collection.deleteOne(Filters.eq("id", id));
 
 	}
 
--- src/test/java/it/pietro/subscriptionsmanager/repository/mongo/SubscriptionMongoRepositoryTest.java
+++ src/test/java/it/pietro/subscriptionsmanager/repository/mongo/SubscriptionMongoRepositoryTest.java
@@ -1,6 +1,6 @@
 package it.pietro.subscriptionsmanager.repository.mongo;
 
-import static org.junit.Assert.*;
+import static org.assertj.core.api.Assertions.*;
 
 import org.bson.Document;
 import org.junit.After;
@@ -15,9 +15,13 @@ import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 
 import java.net.InetSocketAddress;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
+
 import de.bwaldvogel.mongo.MongoServer;
 import de.bwaldvogel.mongo.backend.memory.MemoryBackend;
-import it.pietro.subscriptionsmanager.repository.mongo.SubscriptionMongoRepository;
+import it.pietro.subscriptionsmanager.model.Subscription;
 
 public class SubscriptionMongoRepositoryTest {
 	
@@ -27,6 +31,9 @@ public class SubscriptionMongoRepositoryTest {
 	private MongoClient client;
 	private SubscriptionMongoRepository repository;
 	private MongoCollection<Document> collection;
+	
+	private static final Subscription SUBSCRIPTION_FIXTURE = new Subscription("1", "Netflix", 1.0, "Month");
+	private static final Subscription SUBSCRIPTION_FIXTURE2 = new Subscription("2", "Test", 1.0, "Month");
 	
 	@BeforeClass
 	public static void setupServer() {
@@ -53,9 +60,62 @@ public class SubscriptionMongoRepositoryTest {
 		client.close();
 	}
 
+	public void testFindAllWhenDatabaseIsEmpty() {
+		assertThat(repository.findAll()).isEmpty();
+	}
+	
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testFindAllWhenDatabaseIsNotEmpty() {
+		addTestSubscriptiontToDatabase(SUBSCRIPTION_FIXTURE);
+		addTestSubscriptiontToDatabase(SUBSCRIPTION_FIXTURE2);
+		assertThat(repository.findAll())
+				.containsExactly(
+						SUBSCRIPTION_FIXTURE,
+						SUBSCRIPTION_FIXTURE2);
+	}
+	
+	@Test
+	public void testFindByIdWhenSubscriptionFound() {
+		addTestSubscriptiontToDatabase(SUBSCRIPTION_FIXTURE2);
+		assertThat(repository.findById(SUBSCRIPTION_FIXTURE2.getId()))
+				.isEqualTo(SUBSCRIPTION_FIXTURE2);
+	}
+	
+	@Test
+	public void testFindByWhenSubscriptionNotFound() {
+		assertThat(repository.findById(SUBSCRIPTION_FIXTURE.getId()))
+				.isNull();
+	}
+	
+	@Test
+	public void testSave() {
+		repository.save(SUBSCRIPTION_FIXTURE);
+		assertThat(readAllSubscriptionFormDB())
+			.containsExactly(SUBSCRIPTION_FIXTURE);
+				
+	}
+	
+	@Test
+	public void testDelete() {
+		addTestSubscriptiontToDatabase(SUBSCRIPTION_FIXTURE);
+		repository.delete(SUBSCRIPTION_FIXTURE.getId());
+		assertThat(readAllSubscriptionFormDB()).isEmpty();
+	}
+	
+	private void addTestSubscriptiontToDatabase(Subscription sub) {
+		collection.insertOne(
+				new Document()
+					.append("id", sub.getId())
+					.append("name", sub.getName())
+					.append("price", sub.getPrice())
+					.append("repetition", sub.getRepetition()));
+	}
+	
+	private List<Subscription> readAllSubscriptionFormDB() {
+		return StreamSupport
+				.stream(collection.find().spliterator(), false)
+				.map(d -> new Subscription(""+d.get("id"), ""+d.get("name"), (double)d.get("price"), ""+d.get("repetition")))
+				.collect(Collectors.toList());
 	}
 
 }
