c45effe2dab0af638dc5b677df2f26274fb40f1f
Pietro Zarri <pietrozarri96@gmail.com>
2021-01-21 12:23:59 +0100

    fix bugs

--- src/main/java/it/pietro/subscriptionmanager/spending/SubscriptionSpending.java
+++ src/main/java/it/pietro/subscriptionmanager/spending/SubscriptionSpending.java
@@ -5,14 +5,12 @@ import it.pietro.subscriptionsmanager.model.Subscription;
 
 public class SubscriptionSpending {
 	
-	public SubscriptionSpending() {};
-	
-	public double computeSpending(List<Subscription> subs) {
+	public static double computeSpending(List<Subscription> subs) {
 		Double price = 0.0;
 		for(Subscription sub : subs) {
-			if (sub.getRepetition() == "Weekly") {
+			if (sub.getRepetition().equals("Weekly")) {
 				price += sub.getPrice()*4;
-			} else if( sub.getRepetition() == "Monthly") {
+			} else if( sub.getRepetition().equals("Monthly")) {
 				price += sub.getPrice();
 			} else {
 				price += sub.getPrice()/12;
--- src/test/java/it/pietro/subscriptionmanager/spending/SubscriptionSpendingTest.java
+++ src/test/java/it/pietro/subscriptionmanager/spending/SubscriptionSpendingTest.java
@@ -13,36 +13,34 @@ public class SubscriptionSpendingTest {
 	Subscription MONTHLY_SUB_FIXTURE = new Subscription("0", "Netflix", 1.0, "Monthly");
 	Subscription WEEKLY_SUB_FIXTURE = new Subscription("1", "Magazine", 2.0, "Weekly");
 	Subscription ANNUAL_SUB_FIXTURE = new Subscription("2", "Gym", 120.0, "Annual");
-	
-	private SubscriptionSpending spending = new SubscriptionSpending();
 
 	@Test
 	public void testComputeSpendindWhenThereAreNoSubscrptions() {
 		List<Subscription> subs = Collections.emptyList();
-		assertThat(spending.computeSpending(subs)).isEqualTo(0);
+		assertThat(SubscriptionSpending.computeSpending(subs)).isEqualTo(0);
 	}
 	
 	@Test
 	public void testComputeSpendingWhenThereIsOneMonthlySubscription() { 
-		assertThat(spending.computeSpending(asList(MONTHLY_SUB_FIXTURE)))
+		assertThat(SubscriptionSpending.computeSpending(asList(MONTHLY_SUB_FIXTURE)))
 			.isEqualTo(MONTHLY_SUB_FIXTURE.getPrice());
 	}
 	
 	@Test
 	public void testComputeSpndingWhenThereIsOneWeeklySubscription() {
-		assertThat(spending.computeSpending(asList(WEEKLY_SUB_FIXTURE)))
+		assertThat(SubscriptionSpending.computeSpending(asList(WEEKLY_SUB_FIXTURE)))
 			.isEqualTo(WEEKLY_SUB_FIXTURE.getPrice()*4);
 	}
 	
 	@Test
 	public void testComputeSpndingWhenThereIsOneAnnualSubscription() {
-		assertThat(spending.computeSpending(asList(ANNUAL_SUB_FIXTURE)))
+		assertThat(SubscriptionSpending.computeSpending(asList(ANNUAL_SUB_FIXTURE)))
 			.isEqualTo(ANNUAL_SUB_FIXTURE.getPrice()/12);
 	}
 	
 	@Test
 	public void testComputeSpendingWhenThereAreDifferentaSubscripitons() {
-		assertThat(spending.computeSpending(asList(WEEKLY_SUB_FIXTURE, MONTHLY_SUB_FIXTURE, ANNUAL_SUB_FIXTURE)))
+		assertThat(SubscriptionSpending.computeSpending(asList(WEEKLY_SUB_FIXTURE, MONTHLY_SUB_FIXTURE, ANNUAL_SUB_FIXTURE)))
 			.isEqualTo(19.0);
 	}
 
