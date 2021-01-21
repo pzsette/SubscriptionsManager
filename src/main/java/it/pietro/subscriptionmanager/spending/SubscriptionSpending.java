f14319d39ebeba481a4efa1597dd08a95b01f1fc
Pietro Zarri <pietrozarri96@gmail.com>
2021-01-21 10:40:30 +0100

    add SubscriptionSepending class
    and relative tests to the algorithm

---
+++ src/main/java/it/pietro/subscriptionmanager/spending/SubscriptionSpending.java
@@ -0,0 +1,24 @@
+package it.pietro.subscriptionmanager.spending;
+
+import java.util.List;
+import it.pietro.subscriptionsmanager.model.Subscription;
+
+public class SubscriptionSpending {
+	
+	public SubscriptionSpending() {};
+	
+	public double computeSpending(List<Subscription> subs) {
+		Double price = 0.0;
+		for(Subscription sub : subs) {
+			if (sub.getRepetition() == "Weekly") {
+				price += sub.getPrice()*4;
+			} else if( sub.getRepetition() == "Monthly") {
+				price += sub.getPrice();
+			} else {
+				price = sub.getPrice()/12;
+			}
+		}
+		return price;
+ 	}
+
+}
