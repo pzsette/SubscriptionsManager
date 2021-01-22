665ddaee83e5cae2360e6db7317247542d6360c7
Pietro Zarri <pietrozarri96@gmail.com>
2021-01-22 10:49:28 +0100

    add GUIActionRunnner execute in test

--- src/test/java/it/pietro/subscrptionsmanager/view/swing/SubscriptionViewSwingTest.java
+++ src/test/java/it/pietro/subscrptionsmanager/view/swing/SubscriptionViewSwingTest.java
@@ -1,15 +1,34 @@
 package it.pietro.subscrptionsmanager.view.swing;
 
+import org.assertj.swing.edt.GuiActionRunner;
+import org.assertj.swing.fixture.FrameFixture;
 import org.assertj.swing.junit.runner.GUITestRunner;
 import org.assertj.swing.junit.testcase.AssertJSwingJUnitTestCase;
+import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import it.pietro.subscriptionsmanager.view.swing.SubscriptionViewSwing;
 
 @RunWith(GUITestRunner.class)
 public class SubscriptionViewSwingTest extends AssertJSwingJUnitTestCase {
+	
+	private FrameFixture windows;
+	
+	private SubscriptionViewSwing swingView;
 
 	@Override
 	protected void onSetUp() throws Exception {
-		// TODO Auto-generated method stub
+		GuiActionRunner.execute(() -> {
+			swingView = new SubscriptionViewSwing();
+			return swingView;
+		});
+		windows = new FrameFixture(robot(), swingView);
+		windows.show();
+	}
+	
+	@Test
+	public void test() {
+		//test
 	}
 
 }
